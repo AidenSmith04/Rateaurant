d8bca19786bec87ecf386b058a87232af26e2984
Sami Rahim <2633941r>
2023-03-05 21:51:41 +0000

    refactored models.py to make use of django User class and not store passwords

--- project/views.py
+++ project/views.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from project.models import Restaurant
+from project.forms import CustomerForm, OwnerForm, RestaurantForm, UserForm
+from Populate_Rateaurant import generateID
 
 
 def home(request):
@@ -17,8 +20,32 @@ def categories(request):
 
 
 def register(request):
-    response = render(request, 'Rateaurant/Register.html')
-    return response
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        customer_form = CustomerForm(request.POST)
+        owner_form = OwnerForm(request.POST)
+
+        if customer_form.is_valid():
+            customer = customer_form.save(commit=False)
+            customer.customer_ID = generateID()
+            login(request.P)
+            registered = True
+        elif owner_form.is_valid():
+            owner = owner_form.save(commit=False)
+            owner.owner_ID = generateID()
+            owner.save()
+
+            registered = True
+        else:
+            print(customer_form.errors, owner_form.errors)
+    else:
+        customer_form = CustomerForm()
+        owner_form = OwnerForm()
+
+    context = {'customer_form': customer_form, 'owner_form': owner_form, 'registered': registered}
+    return render(request, 'Rateaurant/Register.html', context=context)
 
 
 def login(request):
